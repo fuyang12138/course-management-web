d66ac09ed9ba6ec1e5418551e8c31cf727d4c7c6
fuyang <2941473574@qq.com>
2024-12-21 15:08:04 +0800

    增加了教师页面得展示

--- teacher_routes.py
+++ teacher_routes.py
@@ -1,21 +1,27 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import HTMLResponse
 
 import pandas as pd
 
-from config import INFO_DB_PATH
+from config import USER_DB_PATH, INFO_DB_PATH
 from helpers import templates
 
 router = APIRouter()
 
 
 @router.get('/teachers', response_class=HTMLResponse)
-async def teachers(request: Request):
+async def teachers_(request: Request):
     """
     教师列表页面
     :param request: 请求对象
     :return: 渲染的教师列表页面
     """
-    df_info = pd.read_csv(INFO_DB_PATH)
-    teachers = df_info[df_info['role'] == 'teacher'].to_dict('records')
+    try:
+        df_user = pd.read_csv(USER_DB_PATH)
+        df_info = pd.read_csv(INFO_DB_PATH)
+        teacher_username = df_user[df_user['role'] == 'teacher']['username'].tolist()
+
+        teachers = df_info[df_info['username'].isin(teacher_username)].to_dict('records')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'无法读取文件：{e}')
     return templates.TemplateResponse('teachers.html', {'request': request, 'teachers': teachers})
